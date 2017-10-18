ebb2d588819fcce3f5bbe76ece9b857bf4bbc865
Shimi Sheetrit <shimi.s@matomy.com>
2017-10-18 15:04:13 +0300

    fix for adsapce_strict in banner

--- MobFoxSDKCoreDynamic.embeddedframework/MobFoxSDKCoreDynamic.framework/Headers/MFConstants.h
+++ MobFoxSDKCoreDynamic.embeddedframework/MobFoxSDKCoreDynamic.framework/Headers/MFConstants.h
@@ -14,7 +14,7 @@ extern NSString* const CORE_TARGET;
 
 
 /* Versions */
-#define SDK_VERSION @"Core_3.1.9"
+#define SDK_VERSION @"Core_3.1.9b"
 #define OS_VERSION        [[[UIDevice currentDevice] systemVersion] floatValue]
 #define FW_VERSION        [[NSBundle mainBundle] objectForInfoDictionaryKey: @"CFBundleShortVersionString"]
 #define BUILD_VERSION     [[NSBundle mainBundle] objectForInfoDictionaryKey: (NSString *)kCFBundleVersionKey]
